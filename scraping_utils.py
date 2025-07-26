7732c692eea31432b212dd0f5e5f0e69b109db65
Yun-Fei Liu <takualiu@gmail.com>
2025-07-26 07:59:33 -0400

    try downloading zip files
    seeking to download data zip files directly from Cochrane URL. Located the URL, but still haven't found a way to download it

--- scraping_utils.py
+++ scraping_utils.py
@@ -5,18 +5,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+import requests
+
 import time
 import re
 import pandas as pd
 from datetime import datetime
+import os
 
 options = Options()
-options.add_argument('--headless')  # Run without GUI
+options.add_argument('--headless=new')  # Run without GUI
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--window-size=1920,1080")
 
-def scrape_review_data(url, driver_options=options):
+DL_dir = "C:/Users/PC/Dropbox/various_projects/retraction_watch/MEP_DataAnalyst/shared_code"
+
+
+def scrape_review_data(url, driver_options=options, DL_dir=DL_dir):
+    options.add_experimental_option("prefs", {
+    "download.default_directory": DL_dir,
+    "download.prompt_for_download": False,
+    "directory_upgrade": True
+       })
+    
     vals = {
         "guideline":0,
         "pub_date":None,
@@ -48,8 +61,60 @@ def scrape_review_data(url, driver_options=options):
     gline_ct = driver.find_element(By.CSS_SELECTOR, 'a[title="Guidelines"]').text
     pub_date = driver.find_element(By.CSS_SELECTOR, 'span.publish-date').text
     pub_date = datetime.strptime(pub_date.split(": ")[-1], "%d %B %Y").date()
-    # gline_ct = get_guideline_text_safe(driver)
+    
+    DL_link = driver.find_element(By.CSS_SELECTOR, "li.download-stats-data-link")
+    DL_link.click()
     driver.save_screenshot("debug_html.png")
+    DL_URL = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "form.download-stats-data-form")))
+    DL_URL = DL_URL.get_attribute("action")
+    
+    DL_URL = DL_URL.split("?")[0]
+    print(DL_URL)
+    pkg_name = DL_URL.split("/")[-1]
+    
+    DL_driver = webdriver.Chrome(options=options)
+    # DL_driver.execute_cdp_cmd(
+    # "Page.setDownloadBehavior",
+    # {
+        # "behavior": "allow",
+        # "downloadPath": DL_dir
+    # }
+    # )
+    DL_URL = "https://www.cochranelibrary.com/cdsr/doi/10.1002/14651858.CD014624.pub2/media/CDSR/CD014624/supinfo/CD014624-dataPackage.zip"
+    DL_driver.get(DL_URL)
+    # wait_for_download(DL_dir)
+    time.sleep(5)
+    DL_driver.save_screenshot("debug_DL_html.png")
+    DL_driver.quit()
+    
+    # selenium_cookies = driver.get_cookies()
+    # cookies = {cookie['name']: cookie['value'] for cookie in selenium_cookies}
+    # headers = {
+    # "User-Agent": driver.execute_script("return navigator.userAgent"),
+    # "Referer": driver.current_url,
+    # "Accept": "*/*",
+    # "Accept-Language": "en-US,en;q=0.5",
+    # "Connection": "keep-alive"
+        # }
+
+
+    
+    # with requests.get(DL_URL, headers=headers, cookies=cookies, stream=True) as r:
+        # print(r.headers.get("Content-Type"))
+        # r.raise_for_status()
+        # with open(pkg_name, "wb") as f:
+            # for chunk in r.iter_content(chunk_size=8192):
+                # if chunk:
+                    # f.write(chunk)
+    # DL_file = requests.get(DL_URL)
+    # with open(pkg_name, "wb") as f:
+        # f.write(DL_file.content)
+    
+    time.sleep(2)
+    
+    
+    # gline_ct = get_guideline_text_safe(driver)
+    
     print(f"... published on {pub_date}. {gline_ct}")
     if len(gline_ct)>0:
         vals["guideline"] = int(gline_ct.split(" ")[2])
@@ -61,3 +126,5 @@ def scrape_review_data(url, driver_options=options):
     driver.quit()
     time.sleep(3) # to avoid being regarded as a DDoS attack
     return vals
+    
+# <form class="download-stats-data-form" method="GET" target="_blank" action="/cdsr/doi/10.1002/14651858.CD014624.pub2/media/CDSR/CD014624/supinfo/CD014624-dataPackage.zip?content-disposition=attachment&amp;mime-type=application/octet-stream"> <input type="hidden" name="content-disposition" value="attachment"> <input type="hidden" name="mime-type" value="application/octet-stream"> <p class="print-options-controls"> <input type="checkbox" class="download-stats-data-toc-check-box" aria-label="I agree to these terms and conditions"> <span class="checkbox-label" aria-hidden="true">I agree to these terms and conditions</span> <button class="btn primary download-stats-data pull-right" disabled=""><i class="fa fa-download" aria-hidden="true"></i> Download data</button> </p> </form>
